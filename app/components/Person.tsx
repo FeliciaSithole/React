269d51b7038dc6887d9419b477d259725b8b15a4
Felicia Sithole <feliciasithole.fs@gmail.com>
2025-07-04 13:48:16 +0200

    initial commit

---
+++ app/components/Person.tsx
@@ -0,0 +1,13 @@
+import Person from "../types/Person"
+export default function Profile({person}): {person: Person}
+
+(){
+    return (
+        <>
+        <h1>{Person.name}</h1>
+        <img
+        className="avatar"
+        src= {Person. }
+        </>
+    )
+}
